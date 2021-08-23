b712ed6132cc9ab8b59185100b74d6be19fb8ade
Youssef <youssef.marzouk@esprit.tn>
2021-08-23 16:51:57 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('Formation flutter'),
+          ),
+          body: Center(
+            child: Text("Hello Sirat"),
+          )),
+    );
+  }
+}
